30bfb0f5d8a7200f33f6077060a08946c43fd561
Jamie Taylor <seppydude@gmail.com>
2017-05-20 00:24:13 +0100

    Added PasswordGenerator.Core repo

---
+++ PasswordGenerator.Core/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PasswordGeneratorCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the PasswordGenerator (which is a .NET Framework package)
+            var pwGen = new PasswordGenerator()
+            // use PasswordGenerator's Fluent Api to include the character options
+                            .IncludeLowercase()
+                            .IncludeNumeric()
+                            .IncludeSpecial()
+                            .IncludeUppercase();
+            // generate a password using the .NET Framework package
+            var newPassword = pwGen.Next();
+
+            Console.WriteLine($"Generated password is: {newPassword}");
+        }
+    }
+}
